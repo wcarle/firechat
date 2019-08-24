a51976b7ecfe73ccd0316826af74a2da91a544d0
Will Carle <william.carle@cbsinteractive.com>
2019-08-23 17:22:56 -0700

    Wrap in anon function

--- public/js/firechat.js
+++ public/js/firechat.js
@@ -1,15 +1,16 @@
-// Set the configuration for your app
-// TODO: Replace with your project's config object
-var config = {
-    apiKey: "apiKey",
-    authDomain: "wcarle-firechat.firebaseapp.com",
-    databaseURL: "https://wcarle-firechat.firebaseio.com",
-};
-firebase.initializeApp(config);
+(function() {
+    // Set the configuration for your app
+    var config = {
+        apiKey: "apiKey",
+        authDomain: "wcarle-firechat.firebaseapp.com",
+        databaseURL: "https://wcarle-firechat.firebaseio.com",
+    };
+    firebase.initializeApp(config);
 
-// Get a reference to the database service
-var database = firebase.database();
+    // Get a reference to the database service
+    var database = firebase.database();
 
-database.ref('test').set({
-    hello: 'world'
-});
+    database.ref('test').set({
+        hello: 'world'
+    });
+})();
